f0f8c9f34949eca49ed39b00518f30e49547bdc2
Gor <7495500@gmail.com>
2023-02-17 16:57:09 +0300

    Basic styles

---
+++ app/controllers/addresses_controller.rb
@@ -0,0 +1,53 @@
+class AddressesController < ApplicationController
+  before_action :set_address, only: %i[ show edit update destroy ]
+
+  def index
+    @addresses = Address.all
+  end
+
+  def show; end
+  def edit; end
+
+  def new
+    @address = Address.new
+  end
+
+  def create
+    @address = Address.new(address_params)
+
+    respond_to do |format|
+      if @address.save
+        format.html { redirect_to address_url(@address) }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @address.update(address_params)
+        format.html { redirect_to address_url(@address) }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @address.destroy
+    respond_to do |format|
+      format.html { redirect_to addresses_url }
+    end
+  end
+
+  private
+
+  def set_address
+    @address = Address.find(params[:id])
+  end
+
+  def address_params
+    params.require(:address).permit(:country, :state)
+  end
+end
